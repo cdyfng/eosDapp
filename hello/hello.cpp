791cb3c36717e1a62283f9dfd5657c65a5a799a5
cdyfng <xiaofeng_chen2007@163.com>
2019-03-06 23:28:06 +0800

    hello on cdt1.5

---
+++ hello/hello.cpp
@@ -0,0 +1,16 @@
+#include <eosiolib/eosio.hpp>
+
+using namespace eosio;
+
+class [[eosio::contract("hello")]] hello : public contract {
+  public:
+      using contract::contract;
+
+      [[eosio::action]]
+      void hi( name user ) {
+         print( "Hello, ", user);
+      }
+};
+
+EOSIO_DISPATCH( hello, (hi))
+//eosio-cpp -o hello.wasm hello.cpp --abigen
